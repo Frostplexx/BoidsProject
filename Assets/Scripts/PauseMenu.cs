7fe0c92c97b0d49e0d88b76a2d538da09b58c03e
Flameplexx <Flameplexx@users.noreply.github.com>
2020-05-20 09:45:36 +0200

    stuff i did yesterday, now finishing pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    public bool paused;
+    public GameObject pauseUI;
+
+    void Update()
+    {
+        
+    }
+
+    void Unpause()
+    {
+        pauseUI.SetActive(false);
+        Time.timeScale = 1f;
+        paused = false;
+
+    }
+
+    void Pause()
+    {
+        pauseUI.SetActive(true);
+        Time.timeScale = 0f;
+        paused = true;
+    }
+
+    void goToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
